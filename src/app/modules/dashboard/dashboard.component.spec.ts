4f46f77de623d8e01838cf267a556875de0e272b
Nicollas Santos <nicollas.comp@gmail.com>
2024-08-02 08:07:23 -0300

    Creation of unit tests. General testing in the application. Revision.

--- src/app/modules/dashboard/dashboard.component.spec.ts
+++ src/app/modules/dashboard/dashboard.component.spec.ts
@@ -1,17 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  inject,
+  waitForAsync,
+} from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrService, provideToastr } from 'ngx-toastr';
+
+import { DeliveryService } from '@services/delivery/delivery.service';
+import * as mockData from '@services/mock_test.json';
 
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let service: DeliveryService;
+  let toast: ToastrService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DashboardComponent]
-    })
-    .compileComponents();
-    
+      imports: [DashboardComponent, HttpClientModule, BrowserAnimationsModule],
+      providers: [provideToastr()],
+    }).compileComponents();
+
+    toast = TestBed.inject(ToastrService);
+    service = TestBed.inject(DeliveryService);
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +35,17 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should use the service on change', () => {
+    const fakedFetchedList = mockData;
+
+    waitForAsync(
+      inject([DeliveryService], (service: any) =>
+        service
+          .getDeliveryList()
+          .subscribe((result: any) => expect(result).toBe(fakedFetchedList)),
+      ),
+    );
   });
 });
--- src/app/modules/delivery-list/delivery-list.component.spec.ts
+++ src/app/modules/delivery-list/delivery-list.component.spec.ts
@@ -1,23 +1,96 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  inject,
+  TestBed,
+  waitForAsync,
+} from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSelectHarness } from '@angular/material/select/testing';
+
+import { ToastrService, ToastrModule, provideToastr } from 'ngx-toastr';
+
+import { DeliveryService } from '@services/delivery/delivery.service';
+import * as mockData from '@services/mock_test.json';
 
 import { DeliveryListComponent } from './delivery-list.component';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
 
 describe('DeliveryListComponent', () => {
   let component: DeliveryListComponent;
   let fixture: ComponentFixture<DeliveryListComponent>;
+  let loader: HarnessLoader;
+  let service: DeliveryService;
+  let toast: ToastrService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DeliveryListComponent]
-    })
-    .compileComponents();
-    
+      imports: [
+        DeliveryListComponent,
+        BrowserAnimationsModule,
+        ToastrModule,
+        HttpClientModule,
+      ],
+      providers: [provideToastr()],
+    }).compileComponents();
+
+    toast = TestBed.inject(ToastrService);
+    service = TestBed.inject(DeliveryService);
     fixture = TestBed.createComponent(DeliveryListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('on change motorista', () => {
+    component.onChangeDriver({ target: { value: 'Carlos' } });
+    fixture.detectChanges();
+  });
+
+  it('should use the service', () => {
+    const fakedFetchedList = mockData;
+
+    waitForAsync(
+      inject([DeliveryService], (service: any) =>
+        service
+          .getDeliveryList()
+          .subscribe((result: any) => expect(result).toBe(fakedFetchedList)),
+      ),
+    );
+  });
+
+  it('should trigger onChangeDriver', async(() => {
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      let field: HTMLInputElement = fixture.debugElement.query(
+        By.css('#filter-driver'),
+      ).nativeElement;
+
+      expect(field.value).toBe('');
+
+      field.value = 'Carlos';
+      field.dispatchEvent(new Event('input'));
+
+      fixture.detectChanges();
+
+      expect(component.filterDriver).toBe('Carlos');
+    });
+  }));
+
+  it('should trigger onChangeStatus', async () => {
+    expect(component.filterStatus).toBe('');
+    const select = await loader.getHarness(MatSelectHarness);
+    await select.open();
+    const bugOption = await select.getOptions({ text: 'ENTREGUE' });
+    await bugOption[0].click();
+
+    expect(component.filterStatus).toBe('ENTREGUE');
   });
 });
